fdddabcfa61e40e5d9f5fb28818bc57896404a8e
vitor-costa <v2costa@gmail.com>
2015-04-18 21:19:43 -0300

    Função de execução parcial do Fisher-Yates Shuffle

--- js/steganography.js
+++ js/steganography.js
@@ -1,4 +1,4 @@
-function convert_text_to_binary(text) {
+function convertTextToBinary(text) {
   var  binary = "";
     for (i=0; i < text.length; i++) {
         binary += byteString(text[i]);
@@ -11,11 +11,45 @@ function byteString(n) {
 	return ("000000000" + n.charCodeAt(0).toString(2)).substr(-8)
 }
 
-function prepare_metadata(data, extension) {
-	var length = convert_text_to_binary(data.length.toString());
-	var ext = convert_text_to_binary(extension);
+function prepareMetadata(data, extension) {
+	var length = convertTextToBinary(data.length.toString());
+	var ext = convertTextToBinary(extension);
 	var binary;
 	var terminator = "00010100";
 	binary = length + terminator + ext + terminator;
 	return binary;
+}
+
+function generateRandomPermutation(totalNumberOfElements, numberOfPermutations, seed) {
+	var crypto = [];
+	// StringBuilder sb = new StringBuilder();
+	var random = Math.seedrandom(seed);
+
+	var imageArray = [];
+	// int[] imageArray = new int[totalNumberOfElements];
+	for(var i = 0; i < totalNumberOfElements; i++) {
+		imageArray.push(i);
+	}
+
+	if(numberOfPermutations > totalNumberOfElements) {
+		numberOfPermutations = totalNumberOfElements;
+	}
+
+	for(var i = 0; i < numberOfPermutations; i++) {
+		var aux = imageArray[i];
+		// var generatedNumber = i + random.nextInt(Integer.MAX_VALUE) % ((totalNumberOfElements) - i);
+		var generatedNumber = 1 + getRandomArbitrary(0, Number.MAX_VALUE) % ((totalNumberOfElements) - i);
+		imageArray[i] = imageArray[generatedNumber];
+		imageArray[generatedNumber] = aux;
+	}
+
+	for(var i = 0; i < imageArray.length; i++) {
+		crypto.push(imageArray[i]);
+	}
+
+	return crypto;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
